463097fc05cf49ff76b92f250617463d89148699
Emmanuel Okorie <teymee17@gmail.com>
2023-06-01 15:38:52 +0100

    update

--- src/pages/customer/StationLocator/index.jsx
+++ src/pages/customer/StationLocator/index.jsx
@@ -19,7 +19,9 @@ export default function StationLocator() {
       );
       const data = await response.json();
 
-      setAddress(`${data.address.city ? data.address.city : ""}, ${data.address.state}`);
+
+
+      setAddress(`${data.address.city ? `${data.address.city} ${data.address.state}` : "Unknown"}`);
       
     } catch (error) {
       console.log("Error getting address:", error);
